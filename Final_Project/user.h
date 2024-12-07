89deaa94a2f0f05262f9945fd87703fa23d8b68c
Nguyen Van Thanh Dat <ndat44892@gmail.com>
2024-12-07 16:14:59 +0700

    Checkpoint2

--- Final_Project/user.h
+++
@@ -1,8 +0,0 @@
-#ifndef USER_H
-#define USER_H
-
-int user_exists(const char* username);
-int register_user(const char* username, const char* password);
-int authenticate_user(const char* username, const char* password);
-
-#endif
